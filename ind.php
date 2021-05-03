46f27146a7eb905cb97a7e23c4a395e29dc8dfe5
rabiausman61 <81038373+rabiausman61@users.noreply.github.com>
2021-05-03 22:47:27 +0500

    ass

---
+++ ind.php
@@ -0,0 +1,1 @@
+<?php include("./assignment.html") ?>
---
+++ jque.js
@@ -0,0 +1,65 @@
+$(function()
+{
+    $("#add").click(productUpdate);
+    $("#reset").click(formClear);
+    $("#update").click(productUpdate);
+   
+  
+});
+
+function productsAdd() {
+    $("#m").append("<tr>" +
+        "<td>" + $("#fname").val() + "</td>"  +
+        "<td>" + $("#age").val() + "</td>" +
+        "<td>" +$("input[name='gender']:checked").val() + "</td>" +
+
+    "<td>" + $("#city option:selected").text() + "</td>" + 
+    "<td>" +
+        "<button id='del' onclick='productdel(this)' > Delete" +
+        "</button>"
+         + 
+        "<button id='up' onclick='' onclick='updatetoform();' > Update" +
+        "</button>"
+         + 
+        "</td>" +
+        "</tr>");
+}
+function productdel(ctl) {
+    $(ctl).parents("tr").remove();
+}
+        
+function productUpdate() {
+   
+    if ($("#name").val() == ("") || $("#age").val()==("") || $("#gender").val()==(""))
+      {
+        alert("Kindly Input All fields"); 
+      }
+    else {
+        productsAdd();
+        formClear();
+      }
+
+
+}
+
+
+function updatetoform(ctl){
+
+        row = $(ctl).parents("tr");
+        var cols = row.children("td");
+        $("fname").val($(cols[1]).text())=$("#m").fname;
+        $("#age").val($(cols[2]).text());
+
+       
+    
+}
+function formClear() {
+    $("#fname").val("");
+    $("#age").val("");
+    $("#gender").val("");
+    $("#city").val("");
+    selectedRow=null;
+  
+}
+var selectedRow = null
+
